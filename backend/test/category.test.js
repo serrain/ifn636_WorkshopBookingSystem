e6c17af389f99c5e0940b748ee1e58cf5c1ca635
boyuan Chen <n11683384@qut.edu.au>
2026-03-28 16:31:25 +1000

    add jest test for category create

--- backend/test/category.test.js
+++ backend/test/category.test.js
@@ -1,6 +1,23 @@
 const request = require('supertest');
 
-const baseURL = "http://localhost:5001"; 
+const baseURL = "http://localhost:5001";
+
+describe('Category API test', () => {
+  it('should be able to successfully create a category', async () => {
+    const newCategory = {
+      category_name: "Programming",
+      description: "Software development and coding workshops"
+    };
+
+    const response = await request(baseURL)
+      .post('/api/categories')
+      .send(newCategory);
+
+    expect(response.statusCode).toBe(201); 
+    expect(response.body).toHaveProperty('_id');
+    expect(response.body.category_name).toBe("Programming");
+  });
+});
 
 describe('Category API test', () => {
   it('be able to successfully get the category list', async () => {
