0bcff3e87b4d75fc99d3fc92883b8bf3893666b0
boyuan Chen <n11683384@qut.edu.au>
2026-03-29 13:18:18 +1000

    modify category test logic based role

--- backend/test/category.test.js
+++ backend/test/category.test.js
@@ -3,61 +3,84 @@ const request = require('supertest');
 const baseURL = "http://localhost:5001";
 
 describe('Category API test', () => {
-  it('should be able to successfully create a category', async () => {
-    const newCategory = {
-      category_name: "Programming" + Date.now(),
-      description: "Software development and coding workshops"
-    };
+    let adminToken;
 
-    const response = await request(baseURL)
-      .post('/api/categories')
-      .send(newCategory);
+    beforeAll(async () => {
+        const loginRes = await request(baseURL)
+            .post('/api/auth/login')
+            .send({
+                email: "admin1@test.com",
+                password: "123456"
+            });
 
-    expect(response.statusCode).toBe(201); 
-    expect(response.body).toHaveProperty('_id');
-  });
-});
+        adminToken = loginRes.body.token;
+    });
 
-describe('Category API test', () => {
-  it('should be able to successfully get the category list', async () => {
-    const response = await request(baseURL).get('/api/categories');
-    expect(response.statusCode).toBe(200);
-  });
-});
+    it('should be able to successfully create a category', async () => {
+        const newCategory = {
+            category_name: "Programming" + Date.now(),
+            description: "Software development and coding workshops"
+        };
 
-describe('Category API test', () => {
-  it('should be able to update an existing category', async () => {
-    const categoryToUpdate = {
-      category_name: "Initial Name" + Date.now(),
-      description: "Initial Description"
-    };
-    const createRes = await request(baseURL).post('/api/categories').send(categoryToUpdate);
-    const categoryId = createRes.body._id;
+        const response = await request(baseURL)
+            .post('/api/categories')
+            .set('Authorization', `Bearer ${adminToken}`)
+            .send(newCategory);
 
-    const updatedData = {
-      category_name: "Programming" + Date.now(),
-      description: "Software development and coding workshops"
-    };
+        expect(response.statusCode).toBe(201);
+        expect(response.body).toHaveProperty('_id');
+    });
 
-    const response = await request(baseURL)
-      .put(`/api/categories/${categoryId}`)
-      .send(updatedData);
 
-    expect(response.statusCode).toBe(200);
-  });
-});
 
-describe('Category API test', () => {
-  it('should be able to delete a category', async () => {
-    const categoryToDelete = { 
-      category_name: "Initial Name" + Date.now(), 
-      description: "Initial Description" 
-    };
-    const createRes = await request(baseURL).post('/api/categories').send(categoryToDelete);
-    const categoryId = createRes.body._id;
+    it('should be able to successfully get the category list', async () => {
+        const response = await request(baseURL).get('/api/categories');
+        expect(response.statusCode).toBe(200);
+    });
 
-    const response = await request(baseURL).delete(`/api/categories/${categoryId}`);
 
-    expect(response.statusCode).toBe(200); 
-  });
+
+    it('should be able to update an existing category', async () => {
+        const categoryToUpdate = {
+            category_name: "Initial Name" + Date.now(),
+            description: "Initial Description"
+        };
+        const createRes = await request(baseURL)
+            .post('/api/categories')
+            .set('Authorization', `Bearer ${adminToken}`)
+            .send(categoryToUpdate);
+        const categoryId = createRes.body._id;
+
+        const updatedData = {
+            category_name: "Programming" + Date.now(),
+            description: "Software development and coding workshops"
+        };
+
+        const response = await request(baseURL)
+            .put(`/api/categories/${categoryId}`)
+            .set('Authorization', `Bearer ${adminToken}`)
+            .send(updatedData);
+
+        expect(response.statusCode).toBe(200);
+    });
+
+
+
+    it('should be able to delete a category', async () => {
+        const categoryToDelete = {
+            category_name: "Initial Name" + Date.now(),
+            description: "Initial Description"
+        };
+        const createRes = await request(baseURL)
+            .post('/api/categories')
+            .set('Authorization', `Bearer ${adminToken}`)
+            .send(categoryToDelete);
+        const categoryId = createRes.body._id;
+
+        const response = await request(baseURL)
+            .delete(`/api/categories/${categoryId}`)
+            .set('Authorization', `Bearer ${adminToken}`);
+
+        expect(response.statusCode).toBe(200);
+    });
 });
